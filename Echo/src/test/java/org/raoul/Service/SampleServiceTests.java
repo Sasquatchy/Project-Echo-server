7b7f7cac4b82e9d662dcb18a4fa5beeb5943d87a
hjunyoung <hjunyoung92@gmail.com>
2019-07-19 15:17:07 +0900

    ??

--- Echo/src/test/java/org/raoul/Service/SampleServiceTests.java
+++ Echo/src/test/java/org/raoul/Service/SampleServiceTests.java
@@ -1,12 +1,17 @@
 package org.raoul.Service;
 
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.raoul.mapper.TimeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-
+import lombok.extern.java.Log;
 import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -14,11 +19,28 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class SampleServiceTests {
 	
-		
-		
-		@Test
-		public void testAdd() throws Exception{
-			
+	@Autowired
+	private DataSource ds;
+	@Autowired
+	private TimeMapper mapper;
+	
+	@Test
+	public void testConnection() {
+		try(Connection con = ds.getConnection()){
+			log.info("-------------");
+			log.info(con);
+			log.info("-------------");
+		}catch(Exception e) {
+			e.printStackTrace();
 		}
-
+	}
+	
+	
+	@Test
+	public void getime() {
+		log.info(mapper.getTime());
+		
+	}
+		
+		
 }
